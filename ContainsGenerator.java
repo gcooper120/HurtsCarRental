734a3e6e61186e9a6f0ce5045a577b3756bde66f
gcooper120 <cooper.george120@gmail.com>
2017-12-28 20:46:13 -0500

    ContainsGen Comments

--- ContainsGenerator.java
+++ ContainsGenerator.java
@@ -2,6 +2,10 @@ import java.sql.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+/**
+*This class is used to generate data for the contains table in the database.
+*For each car in the database, it is assigned to a specific location's inventory
+*/
 public class ContainsGenerator
 {
     //Password Data
@@ -10,9 +14,12 @@ public class ContainsGenerator
     //Main Method
     public static void main(String[] args)
     {
+        //Load jdbc driver
         try {
             Class.forName ("oracle.jdbc.driver.OracleDriver");
         } catch(Exception e){e.printStackTrace();}    
+        
+        //Attempt to run generator, just prints tack traces on errors.
         try (
              Connection con=DriverManager.getConnection("jdbc:oracle:thin:@edgar0.cse.lehigh.edu:1521:cse241",name,password);
              Statement s=con.createStatement();
@@ -20,6 +27,9 @@ public class ContainsGenerator
             String qStandard, qLuxury, qExotic, qLocations;
             ResultSet resultStandard;
             ResultSet resultLuxury, resultExotic, resultLocations;
+
+
+            //Generating lists of the different car types
             List<Integer> listStandard = new ArrayList<Integer>();
             List<Integer> listLuxury = new ArrayList<Integer>();
             List<Integer> listExotic = new ArrayList<Integer>();
@@ -49,6 +59,8 @@ public class ContainsGenerator
                     listLocations.add(resultLocations.getString(1));
 
                 }
+
+            //Generates a random location/car combo
             Random generator = new Random();
             int randLoc, randCar;
             int locSize = listLocations.size();
@@ -60,37 +72,41 @@ public class ContainsGenerator
                 + "(?,?)";
             PreparedStatement prepInsert = con.prepareStatement(insertRow);
             String inventory_ID;
+
+            //Assigns standard cars to their inventories
             for (int i = 0; i < numStandardCars; i++)
-                {
-                    randLoc = generator.nextInt(locSize);
-                    inventory_ID = listLocations.get(randLoc) + "_S";
-                    try {
-                        prepInsert.setString(1, inventory_ID);
-                        prepInsert.setInt(2, listStandard.get(i));
-                        prepInsert.executeUpdate();
-                    } catch (SQLIntegrityConstraintViolationException SQLe){}
-                }
+            {
+                randLoc = generator.nextInt(locSize);
+                inventory_ID = listLocations.get(randLoc) + "_S";
+                try {
+                    prepInsert.setString(1, inventory_ID);
+                    prepInsert.setInt(2, listStandard.get(i));
+                    prepInsert.executeUpdate();
+                } catch (SQLIntegrityConstraintViolationException SQLe){}
+            }
+            //Assigns luxury cars to their inventories
             for (int i = 0; i < numLuxuryCars; i++)
-                {
-                    System.out.println(i);
-                    randLoc = generator.nextInt(locSize);
-                    inventory_ID = listLocations.get(randLoc) + "_L";
-                    try {
-                        prepInsert.setString(1, inventory_ID);
-                        prepInsert.setInt(2, listLuxury.get(i));
-                        prepInsert.executeUpdate();
-                    } catch (SQLIntegrityConstraintViolationException SQLe){}
-                }
+            {
+                System.out.println(i);
+                randLoc = generator.nextInt(locSize);
+                inventory_ID = listLocations.get(randLoc) + "_L";
+                try {
+                    prepInsert.setString(1, inventory_ID);
+                    prepInsert.setInt(2, listLuxury.get(i));
+                    prepInsert.executeUpdate();
+                } catch (SQLIntegrityConstraintViolationException SQLe){}
+            }
+            //Assigns exotic cars to their locations
             for (int i = 0; i < numExoticCars; i++)
-                {
-                    randLoc = generator.nextInt(locSize);
-                    inventory_ID = listLocations.get(randLoc) + "_E";
-                    try {
-                        prepInsert.setString(1, inventory_ID);
-                        prepInsert.setInt(2, listExotic.get(i));
-                        prepInsert.executeUpdate();
-                    } catch (SQLIntegrityConstraintViolationException SQLe){}
-                }
+            {
+                randLoc = generator.nextInt(locSize);
+                inventory_ID = listLocations.get(randLoc) + "_E";
+                try {
+                    prepInsert.setString(1, inventory_ID);
+                    prepInsert.setInt(2, listExotic.get(i));
+                    prepInsert.executeUpdate();
+                } catch (SQLIntegrityConstraintViolationException SQLe){}
+            }
             con.close();
             prepInsert.close();
         } catch(Exception e){e.printStackTrace();}
